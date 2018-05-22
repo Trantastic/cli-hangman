96d03db58a95c05d3082876fb2f4def63f7c4c55
Yvette <yvettetran@gmail.com>
2018-05-22 14:25:59 -0700

    added line of code to print correct letter if present

--- word.js
+++ word.js
@@ -4,12 +4,16 @@ var Word = function() {
 	// Selects random word from wordBank
 	this.randomWord = wordBank[Math.floor(Math.random() * wordBank.length)];
 	// Replaces selected random word with underscores and prints it
+	this.correctLetters = [];
 	this.blankSpaces = function(word) {
 		var arr = word.split("");
 		var blankHolder = [];
 
 		for(var i = 0; i < word.length; i++) {
-			if(arr[i] !== " ") {
+			if(this.correctLetters.includes(arr[i])) {
+				blankHolder.push(arr[i]);
+			}
+			else if(arr[i] !== " ") {
 				blankHolder.push("_");
 			} else {
 				blankHolder.push(" ");
